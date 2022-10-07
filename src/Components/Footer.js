aea5d5344c4477c276d45f5c26ae1321d81518d4
Nikhil Kharana <rituknj@gmail.com>
2022-10-07 14:33:33 +0530

    update-footer

--- src/Components/Footer.js
+++ src/Components/Footer.js
@@ -113,7 +113,7 @@ export default function Footer() {
           </div>
         </div>
       </div>
-      <div className="container">
+      <div className="container12">
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-12 col-12">
             <p className="footer-para-1">
