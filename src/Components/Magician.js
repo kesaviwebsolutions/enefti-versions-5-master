ecb22197cb31080329d5150d4a775f965b5feb7b
gctech22 <108040545+gctech22@users.noreply.github.com>
2022-10-12 14:21:22 +0800

    updated video

--- src/Components/Magician.js
+++ src/Components/Magician.js
@@ -7,17 +7,15 @@ export default function Magician() {
       <div className="container-fluid py-5">
         <div className="row">
           <div className="col-lg-6 col-md-6 col-sm-12 col-12">
-            <p className="head-magi">Return of the Gold Dinar (XAUS)</p>
+            <p className="head-magi">Return of the Gold Dinar</p>
             <p className="magi-para">
-              World's first NFT backed by physical 22 carat purity gold. The
+              XAUS NFTs are world's first NFTs backed by physical 22 carat purity gold. The
               unique XAUS NFT serial number is embossed on a physical gold coin
               of gold weight of 4.25 grams, equivalent to 10 XAUS tokens. From
               the International Halal Assurance Institute, XAUS is the first
               cryptocurrency to receive a Certificate of Sharia Compliance
               (05-March-2022).
             </p>
-          </div>
-          <div className="col-lg-6 col-md-6 col-sm-12 col-12 magi-button">
             <button className="button-magi">
               <a
                 href="https://discord.gg/QNNZGD2Zfa"
@@ -27,6 +25,16 @@ export default function Magician() {
                 Find more on our Discord
               </a>
             </button>
+          </div>
+          <div className="col-lg-6 col-md-6 col-sm-12 col-12">
+            <div>
+                <video loop autoPlay muted className="video">
+                  <source
+                    src={require("../Images/intro-video.mp4")}
+                    type="video/mp4"
+                  />
+                </video>
+              </div>
           </div>
         </div>
       </div>
